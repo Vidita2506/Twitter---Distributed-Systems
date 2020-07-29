ce94bc91538465fb5dce4ff5f1937079b69ec670
Vidita <54425462+Vidita2506@users.noreply.github.com>
2020-07-29 14:39:59 -0700

    Update config.js

--- kafka-backend/utils/config.js
+++ kafka-backend/utils/config.js
@@ -1,10 +1,10 @@
 "use strict";
 module.exports = {
-  mongoDBURI: "mongodb+srv://admin:CMPE273@twitter-ftl7l.mongodb.net/twitter?retryWrites=true&w=majority",
-  kafkaURI: "3.82.252.69:9092,54.197.8.135:9092,3.82.218.63:9092",
+  mongoDBURI: "xxxx",
+  kafkaURI: "xxxx,xxxx,xxxx",
   // Start 3 brokers on local on diff ports for local setup
   //kafkaURI: "localhost:9092,locahost:9093,localhost:9094",
   redisPort: 6379,
   //redisHost: "localhost" // Use this for local redis
-  redisHost: "54.173.125.207"
+  redisHost: "xxxx"
 };
