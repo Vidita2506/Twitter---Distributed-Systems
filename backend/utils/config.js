a21590af1fec05d3e7d9e40d4bca5bec709f87b9
Vidita <54425462+Vidita2506@users.noreply.github.com>
2020-07-29 14:42:23 -0700

    Update config.js

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,14 +2,14 @@
 const config = {
    secret: "cmpe273_kafka_passport_mongo",
    frontendURI: "http://twitter-592983799.us-east-1.elb.amazonaws.com:3000",
-   kafkaURI: "3.82.252.69:9092,54.197.8.135:9092,3.82.218.63:9092",
+   kafkaURI: "xxxx,xxxx,xxxx",
    // Start 3 servers on local on diff ports for local setup
    //kafkaURI: "localhost:9092,locahost:9093,localhost:9094",
    mysqlUser: "root",
-   mysqlPassword: "cmpepassword",
-   mysqlHost: "cmpedatabase.cxwydwkipclw.us-east-1.rds.amazonaws.com",
-   mysqlDatabase: "twitter",
-   awsBucket: "cmpe273twitter",
+   mysqlPassword: "xxxx",
+   mysqlHost: "xxxx",
+   mysqlDatabase: "xxxx",
+   awsBucket: "xxxx",
    // Keys can't be added here because AWS categorizes this as vulnerability.
    awsAccessKey: "xxxx",
    awsSecretAccessKey: "xxxx",
